c489b2fcd4099ee43ea20e12eade26cee6113caf
Dexter49 <dexterousguru49@yahoo.com>
2023-08-05 04:04:12 +0100

    So, I was testing contract deployment

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+require("@nomicfoundation/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env; 
+
+module.exports = {
+  solidity: "0.8.10",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [ PRIVATE_KEY ]
+    }
+  },
+}
